5dca6a70adfd425c675252b757062ea03279e784
Lyor Bech <45292051+Lyor2602@users.noreply.github.com>
2020-12-11 09:54:00 +0100

    1e opdracht

---
+++ 1stgen.py
@@ -0,0 +1,24 @@
+from bke import MLAgent, is_winner, opponent, load, validate, RandomAgent, plot_validation
+ 
+ 
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+ 
+my_agent = load('MyAgent_3000')
+my_agent.learning = False
+ 
+validation_agent = RandomAgent()
+ 
+validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+ 
+plot_validation(validation_result)
+
+print(validation_result)
